d8991e55fdfe8bb1ee0c22b68f7bd5be83ac7918
hzuber-vayyar <59413508+hzuber-vayyar@users.noreply.github.com>
2021-03-25 13:25:34 +0200

    add /users to backend

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3080
+const bodyParser = require("body-parser")
+
+const users=["Hannah", "Alon", "Ohad"]
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send(`Hello World!`)
+  res.send(`Hello World! This is the basic call.`)
+})
+
+app.get('/users', (req, res) => {
+  console.log("/users called!")
+  res.json(users)
 })
 
 app.listen(port, () => {
