207311111244d9ec4ebd2f67fa789aa55962e13d
hzuber-vayyar <59413508+hzuber-vayyar@users.noreply.github.com>
2021-03-25 13:30:51 +0200

    remove bodyparser

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3080
-const bodyParser = require("body-parser")
 
 const users=["Hannah", "Alon", "Ohad"]
-
-app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send(`Hello World! This is the basic call.`)
