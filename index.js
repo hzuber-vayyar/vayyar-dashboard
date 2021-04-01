41d85d1be5b6b164f74c6bc3aa4e8235136e86f0
hzuber-vayyar <59413508+hzuber-vayyar@users.noreply.github.com>
2021-04-01 12:39:49 +0300

    update user objects

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3080
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3080;
 
-const users=["Hannah", "Alon", "Ohad"]
+const users = [
+  { firstName: "Hannah", lastName: "Zuber", email: "hannah.zuber@vayyar.com" },
+  { firstName: "Alon", lastName: "Berman", email: "alon.berman@vayyar.com" },
+  { firstName: "Ohad", lastName: "Petrushka", email: "ohad.petrushka@vayyar.com" },
+];
 
-app.get('/', (req, res) => {
-  res.send(`Hello World! This is the basic call.`)
-})
+app.get("/api/", (req, res) => {
+  res.send(`Hello World! This is the basic call.`);
+});
 
-app.get('/users', (req, res) => {
-  console.log("/users called!")
-  res.json(users)
-})
+app.get("/api/users", (req, res) => {
+  console.log("/users called!");
+  res.json(users);
+});
+
+app.post('/api/user', function (req, res) {
+  console.log("body is ", req.body)
+  const user = req.body;
+  console.log('Adding user:::::', user);
+  users.push(user);
+  console.log("all users: ", users)
+  res.json("user added");
+});
 
 app.listen(port, () => {
-  console.log(`Vayyar node app listening at http://localhost:${port}`)
-})
+  console.log(`Vayyar node app listening at http://localhost:${port}`);
+});
